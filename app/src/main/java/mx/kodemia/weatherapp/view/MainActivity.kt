13f004f412872136c93f0a274285c2de3f78529a
FabianGC0610 <fabian.god06@gmail.com>
2022-03-08 12:38:30 -0600

    Creacion de variables globales para el MainActivity

--- app/src/main/java/mx/kodemia/weatherapp/view/MainActivity.kt
+++ app/src/main/java/mx/kodemia/weatherapp/view/MainActivity.kt
@@ -2,11 +2,33 @@ package mx.kodemia.weatherapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import mx.kodemia.weatherapp.R
+import mx.kodemia.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivityError"
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
+    private var latitude = ""
+    private var longitude = ""
+
+    private var units = false
+    private var language = false
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 }
